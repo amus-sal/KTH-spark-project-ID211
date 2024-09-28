c36cd95e850359c21d304136a5ec945ef420f7f5
michailroussos <michael_roussos@yahoo.com>
2024-09-28 20:13:43 +0200

    set up mongo db in a container of the network. adjusted the python script to load the csv in mongodb.

--- data/load_flight_delays.py
+++ data/load_flight_delays.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .appName("Load Flight Delays") \
     .master("spark://spark-master:7077") \
+    .config("spark.mongodb.output.uri", "mongodb://mongo-db:27017/mongodatabase.flightdelays") \
     .getOrCreate()
 
 # Load CSV from HDFS
@@ -11,6 +12,9 @@ df = spark.read.csv("hdfs://hadoop:9000/user/test/data/flight_delays.csv", heade
 
 # Show DataFrame contents
 df.show()
+
+# Save the DataFrame to MongoDB
+df.write.format("mongo").mode("append").save()
 
 # Stop the Spark session
 spark.stop()
